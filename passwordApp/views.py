35709e7cbea4b948a90e8a76dc58d93fd61ee6eb
Marine C <mariine.becker@gmail.com>
2024-02-18 16:13:51 +0100

    formulaire ok

--- passwordApp/views.py
+++ passwordApp/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Password
+from .forms import PasswordForm
 
 def index(request):
     passwords = Password.objects.order_by('-name')
     return render(request, "passwordApp/index.html", {'passwords': passwords})
+
+def create(request):
+    if request.method == 'POST':
+        form = PasswordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = PasswordForm()
+
+    return render(request, 'passwordApp/create.html', {'form': form})
