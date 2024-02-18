04c713446549be5747352325e82b99e2c14497f1
Marine C <mariine.becker@gmail.com>
2024-02-18 18:32:20 +0100

    ajout export csv

--- passwordApp/views.py
+++ passwordApp/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Password
 from .forms import PasswordForm
+import csv
 
 def index(request):
     passwords = Password.objects.order_by('-name')
@@ -33,3 +35,35 @@ def delete(request, password_id):
     password = Password.objects.get(pk=password_id)
     password.delete()
     return redirect('index')
+
+def export_passwords_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="passwords.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Name', 'URL', 'Pseudonym', 'Password'])
+
+    passwords = Password.objects.all()
+    for password in passwords:
+        writer.writerow([password.name, password.url, password.pseudonym, password.password])
+
+    return response
+
+def import_passwords_csv(request):
+    if request.method == 'POST' and request.FILES['csv_file']:
+        csv_file = request.FILES['csv_file']
+        decoded_file = csv_file.read().decode('utf-8').splitlines()
+        reader = csv.DictReader(decoded_file)
+
+        for row in reader:
+            password = Password.objects.create(
+                name=row['Name'],
+                url=row['URL'],
+                pseudonym=row['Pseudonym'],
+                password=row['Password']
+            )
+            password.save()
+
+        return redirect('index')
+
+    return render(request, 'passwordApp/import.html')
